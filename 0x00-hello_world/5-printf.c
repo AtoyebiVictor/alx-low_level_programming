8e94578e7ecbe2a9b74e7da3a3084a3ec478f4db
AtoyebiVictor <vickkypee2001@gmail.com>
2022-06-02 08:51:17 -0700

    lets have all the print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#include <stdio.h>
+
+/**
+ * we are printing
+ * a file and add new
+ * line then return
+ * the value with 0
+ */
+
+int main(void) {
+
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
+
+}
