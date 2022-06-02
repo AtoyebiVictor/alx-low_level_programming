25c23d5814a391f9dd2a55ce6157c17742236fb5
AtoyebiVictor <vickkypee2001@gmail.com>
2022-06-02 08:09:13 -0700

    Writing c codes and print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,14 @@
 #!/bin/bash
-puts "Programming is like building a multilingual puzzle"
+
+#include <stdio.h>
+
+/* print a file
+ * and ends with
+ * value 0
+ */
+
+int main(void) 
+{
+	puts("Programming is like building a multilingual puzzle"/n);
+	return(0);
+}
