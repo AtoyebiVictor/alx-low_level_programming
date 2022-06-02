d9012acc2165105db5b8b9dbfbe5ff08cc207c43
AtoyebiVictor <vickkypee2001@gmail.com>
2022-06-02 15:01:14 -0700

    final

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
-#include <stdio.h>
+#include <unistd.h>
+
 /**
- * main - main block
- *
- * Return: Always 1 (Success)
- */
+  * main - Entry point
+  *
+  * Return: Always 1(Success)
+  */
 int main(void)
 {
-char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\";
-write(2, str1, 70);
-return (1);
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, str1, 59);
+	return (1);
 }
