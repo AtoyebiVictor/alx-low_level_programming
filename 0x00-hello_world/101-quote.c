e496f28d156303a66a29485f6a36bb3f37c199b8
AtoyebiVictor <vickkypee2001@gmail.com>
2022-06-02 15:39:34 -0700

    Final task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 /**
  * main - main block
  *
- * Return: Always 1 (Success)
+ * Return: Always 1(Success)
  */
 int main(void)
 {
 char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(2, str1, 76);
+write(2, str1, 58);
 return (1);
 }
