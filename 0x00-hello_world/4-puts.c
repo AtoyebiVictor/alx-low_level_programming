7454e0ed7ae727969e8ebf4911dcb40b1932af95
AtoyebiVictor <vickkypee2001@gmail.com>
2022-06-02 09:48:24 -0700

    Print

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * We are printing
+ * the file and end 
+ * it with the value
+ * of 0
+ */
+
+int main(void) {
+
+	puts("Programming is like building a multilingual puzzle.\n");
+	return (0);
+
+}
