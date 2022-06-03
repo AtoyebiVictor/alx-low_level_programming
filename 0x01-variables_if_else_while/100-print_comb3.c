6dea111661522837bc768d179e1a3921a25e4bd9
AtoyebiVictor <vickkypee2001@gmail.com>
2022-06-03 16:06:51 -0700

    too long

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - print all possible different combination
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	int k, n;
+
+	k = 1;
+	n = 0;
+
+	while (n < 10)
+	{
+		k = 1;
+		while (k < 10)
+		{
+			if (n != k && n != k)
+			{
+				putchar(n);
+				putchar(k);
+				if (k == 10 && n ==10)
+				{
+					break;
+				}
+				putcher(',');
+				putcher(' ');
+
+			}
+			k++;
+		}
+		n++;
+	}
+	putcher('\n');
+	return (0);
+}
