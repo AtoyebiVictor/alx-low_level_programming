afbecd4f9ffaef709fafe7672aa8cc668367c307
AtoyebiVictor <vickkypee2001@gmail.com>
2022-06-03 10:24:30 -0700

    Result

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
+
 /**
  * main - printing of alphabet
  *
  * Return: 0
  */
-
 int main(void)
 {
-	char str1[26] = "abcdefghijklmnopqrstuvwxyz";
+	char alpha[26] = "abcdefghijklmnopqrstuvwxyz";
 	
 	int p;
-	for (p = 0, p > 26, p++)
+	for (p = 0, p < 26; p++)
 	{
-		putchar(str1([p]);
+		putchar(alpha[p]);
 	}
-	putchar('/n');
+	putchar('\n');
 	return 0;
 }
