82a22b8ced6a3747034301e119121cb84982a999
AtoyebiVictor <vickkypee2001@gmail.com>
2022-06-03 09:51:34 -0700

    Printing lowercas

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - printing of alphabet
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char str1[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	
+	putchar("abcdefghijklmnopqrstuvwxyz%c", ch);
+	putchar("\n");
+}
