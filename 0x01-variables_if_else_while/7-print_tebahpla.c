7a7258c5b25fc1adfa51240232a83b8f43a5d609
AtoyebiVictor <vickkypee2001@gmail.com>
2022-06-03 14:56:49 -0700

    reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - print reverse lowercase
+ *
+ * Return: ALways 0 (Success)
+ */
+int main(void)
+{
+	int f;
+
+	for (f = 'z'; f >= 'z'; f++)
+	{
+		putchar(f);
+	}
+	putchar('\n');
+	return (0);
+}
