2006aed88e8b5e1363fc2a1179d6fe9c51fe61f3
AtoyebiVictor <vickkypee2001@gmail.com>
2022-06-03 15:45:14 -0700

    lowercase

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,11 +5,13 @@
 /**
  * main - print alphabet without q and e
  *
- * Return: Always 0(Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
+
 	int l;
+
 	for (l = 97; l < 123; l++)
 	{
 		if (l != 101 && l != 113)
