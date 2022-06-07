6cbe088bce5285acc5e9c890ff897d42df1e758b
AtoyebiVictor <vickkypee2001@gmail.com>
2022-06-07 09:46:51 -0700

    lets code

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main"
+
+/**
+ * print_alphabet_x10(void) - print lokercase 10 times
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int j;
+	{
+		_putchar("abcdefghijklmnopqrstuvwxyz", j++)
+	}
+	_putchar('\n');
+}
