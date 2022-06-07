e875c1e714b79efe1b6305e570cf384fade8ad78
AtoyebiVictor <vickkypee2001@gmail.com>
2022-06-07 09:30:38 -0700

    Coding

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet in lowercase
+ *
+ * Return: Always 0(SUccess)
+ */
+void print_alphabet(void)
+{
+	int o;
+
+	for (o = 97; o <= 122; o++)
+	{
+		_putchar(o);
+	}
+	_putchar('\n');
+}
