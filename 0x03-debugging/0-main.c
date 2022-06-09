4a47a9f9702c16eee9c182f7b4670de1786b7735
AtoyebiVictor <vickkypee2001@gmail.com>
2022-06-09 03:14:27 -0700

    coding is fun

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - test the function that gives out the correct output
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
