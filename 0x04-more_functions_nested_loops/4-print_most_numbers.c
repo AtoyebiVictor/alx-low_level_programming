15ae03b9b6bb3767db52d3b1623409e93ccd8bd5
AtoyebiVictor <vickkypee2001@gmail.com>
2022-06-09 14:27:17 -0700

    commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,12 +9,10 @@ void print_most_numbers(void)
 {
 	int ch;
 
-	for (ch = 48; ch <= 57; ch++)
+	for (ch = 48; ch < 58; ch++)
 	{
 		if (ch != 50 && ch != 52)
-		{
 			_putchar(ch);
-		}
-		_putchar('\n');
 	}
+	_putchar('\n');
 }
