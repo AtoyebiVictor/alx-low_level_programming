a9212a70b2223af17e0f9e644e507c3d8170e56e
AtoyebiVictor <vickkypee2001@gmail.com>
2022-06-09 16:07:14 -0700

    commit

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,11 +11,11 @@ void print_triangle(int size)
 	
 	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < (size - 1); j++)
+		for (j = 1; j < (size - 1); j++)
 			_putchar(' ');
 		for (j--; j < size; j++)
 			_putchar(35);
-		for (i < (size - 1)
+		if (i < (size - 1))
 				_putchar('\n');
 	}
 	_putchar('\n');
