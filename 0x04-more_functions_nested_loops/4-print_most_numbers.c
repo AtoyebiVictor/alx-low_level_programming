5efe2967a1413c8dc110591ef0e8eafd9de72729
AtoyebiVictor <vickkypee2001@gmail.com>
2022-06-09 14:21:14 -0700

    commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints numbers between 0 to 9.
- * (neither 2 or 4).
- * Return: no return value.
+ * print_most_numbers - prints numbers between 0 to 9
+ * (neither 2 nor 4).
+ * Return: no return.
  */
 void print_most_numbers(void)
 {
 	int j;
+
 	for (j = 0; j <= 9; j++)
 	{
 		if (j != 2 && j != 4)
