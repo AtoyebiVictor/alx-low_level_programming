713838f4470fe3bfff7d88717645da6b4f71566a
AtoyebiVictor <vickkypee2001@gmail.com>
2022-06-09 12:19:09 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Print out the function 
+ * that checks for uppercase character
+ *
+ * Return: 0
+ */
+
+int _isupper(int c)
+{
+	int c;
+
+	if (c = 65; c <= 90; c++)
+	{
+		return (1);
+	}
+	return (0);
+}
