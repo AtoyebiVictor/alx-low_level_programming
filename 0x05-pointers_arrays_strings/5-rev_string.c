53e8b2796f11d584f46c36743068b2ba4aee033a
AtoyebiVictor <vickkypee2001@gmail.com>
2022-06-13 09:30:28 -0700

    commit

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -26,11 +26,15 @@ int _strlen(char *s)
  */
 void rev_string(char *s)
 {
-	int i = 0;
+	char tmp;
+	int i = _strlen(s);
+	int j = 0;
 
-	while (s[i])
+	while (j < i / 2)
 	{
-		i--;
+		tmp = s[j];
+		s[j] = s[i - 1 - j];
+		s[i - 1 - j] = tmp;
+		j++;
 	}
-	return (i);
 }
