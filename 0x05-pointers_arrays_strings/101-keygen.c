180733eeda3c78eff2cb72f45000b3e55a3e9659
AtoyebiVictor <vickkypee2001@gmail.com>
2022-06-13 15:45:40 -0700

    commit

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,5 +1,5 @@
-#include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 /**
  * main - generates random valid passwords
@@ -16,7 +16,7 @@ int main(void)
 	{
 		pass = (rand() % 128);
 		sum += pass;
-		printf("%d", pass);
+		printf("%c", pass);
 	}
 	printf("%c", 2772 - sum);
 
