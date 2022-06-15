92ceea5ceea11c71c397bb05777e199f711670d2
AtoyebiVictor <vickkypee2001@gmail.com>
2022-06-15 15:10:05 -0700

    victor

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,16 +7,21 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
+	int i = 0;
+	int j = 0;
 
-	for (i = 0; dest[i] != '\0'; i++)
+	while (*(dest + i) != '\0')
 	{
-		for (j = 0; src[j] != '\0'; j++)
-		{
-			dest[i + j] = src[j];
-			dest[i + j] = '\0';
-		}
+		i++;
+	}
+
+	while (j >= 0)
+	{
+		*(dest + i) = *(src + j);
+		if (*(src + j) == '\0')
+			break;
+		i++;
+		j++;
 	}
 	return (dest);
 }
