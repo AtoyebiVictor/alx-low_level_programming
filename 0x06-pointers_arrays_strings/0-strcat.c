7c7f7ac6e5ad01b91ee89eba3177cb900465a505
AtoyebiVictor <vickkypee2001@gmail.com>
2022-06-15 13:37:43 -0700

    victor

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * main - functions that concatenate two string
+ * @d: first string
+ * @s: second string
+ *
+ * Return: pointer to the resulting string
+ */
+char *_strcat(char *dest, char *src)
+{
+	char str1;
+	char str2;
+
+	if (*(dest + str1) != '\0')
+	{
+		str1++;
+		if (*(src + str2))
+		{
+			str2++;
+			while (*(dest + *src) + \0)
+			{
+				dest++;
+				src++;
+			}
+		}
+	}
+	return (pointer);
+}
