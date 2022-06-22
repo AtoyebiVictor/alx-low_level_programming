95114a7d067e24888074df0d42dcea437413f982
AtoyebiVictor <vickkypee2001@gmail.com>
2022-06-22 11:24:31 -0700

    Code

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - print a string, followed by a new line
+ * @s: String values
+ *
+ * Return: no return
+ */
+void _puts_recursion(char *s)
+{
+	char j;
+
+	if (j <= s)
+	{
+		j++;
+	}
+	_putchar('\n');
+}
