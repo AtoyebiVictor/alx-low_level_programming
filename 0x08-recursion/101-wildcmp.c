bbc8f5b1c8723d62576640f1e6f3c94533b02be9
AtoyebiVictor <vickkypee2001@gmail.com>
2022-06-23 01:55:35 -0700

    Codes

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -6,18 +6,19 @@
  * @s2: Second string. it can be considered * as a special character
  * Return: 1 if are identical, then 0
  */
+
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == '\0'  && *s2 == '\0')
-		return (1);
-	if (*s1 == *s2)
-		return (wildcmp(s1 + 1, s2 + 1));
-	if (*s2 == '*')
+	if (*s1 == '\0')
 	{
-		if (*(s2 + 1) == '*')
+		if (*s2 != '\0' && *s2 == '*')
 			return (wildcmp(s1, s2 + 1));
-		if (wildcmp(s2, s1 + 1) || wildcmp(s1, s2 + 1))
-			return (0);
+		return (*s2 == '\0');
 	}
+
+	if (*s2 == '*')
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	else if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
 	return (0);
 }
