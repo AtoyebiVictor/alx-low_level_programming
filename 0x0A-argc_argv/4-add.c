bb9d2d7572449219b2ac8af1a9c219c5b566011f
AtoyebiVictor <vickkypee2001@gmail.com>
2022-06-24 13:34:25 -0700

    codes

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,26 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
 /**
  * main - adds positive number
  * @argc: contains the command line of an argument
  * @argv: array that contains the command line of an argument
  * Return: 0 - success
  */
+
 int main(int argc, char *argv[])
 {
-	int i, j, add = 0;
-	
-	for (i = 1; i < argc; i++)
+	int i, j;
+	int sum = 0;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; argv[i]; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (!isdigit(argv[i][j]))
+			if (argv[i][j] < '0' || argv[i][j] > '9')
 			{
 				printf("Error\n");
-				return (i);
+				return (1);
 			}
 		}
-		add += atoi(argv[i]);
+		sum += atoi(argv[i]);
 	}
-	printf("%d", add);
+
+	printf("%d\n", sum);
 	return (0);
 }
