b16ae5546ba2ce4a7af6f4319075c4a44045ca09
AtoyebiVictor <vickkypee2001@gmail.com>
2022-06-24 14:16:46 -0700

    Codes

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -5,7 +5,7 @@
  * main - multiplies two numbers
  * @args: contains the command line of an argument
  * @argv: array of a command line of an argument
- * Return: 0 - success, 1 otherwise
+ * Return: 0 on success, 1 otherwise
  */
 
 int main(int argc, char *argv[])
