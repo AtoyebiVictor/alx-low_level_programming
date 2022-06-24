4e8663c8a1ea657a8f0e4f38b27b6294e0018f15
AtoyebiVictor <vickkypee2001@gmail.com>
2022-06-24 04:30:30 -0700

    Nice work

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name followed by anew line
+ * @argc: containing the argument
+ * @argv: array of the argument
+ * Return: 0
+ */
+int main(int argc((rename)), char* argv[])
+{
+	printf("Name: %i", argv[0]);
+	return (0);
+}
