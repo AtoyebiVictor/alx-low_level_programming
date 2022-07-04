02e67900619b7c5b0a23e151cbc99c0c7626c90b
AtoyebiVictor <vickkypee2001@gmail.com>
2022-07-04 16:02:19 -0700

    lets code

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - prints a struct dog
+ * @d: struct dog
+ *
+ * Return: no value
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		printf(nil);
+#endif
+	if (d == NULL)
+		print('0');
+#endif
+}
