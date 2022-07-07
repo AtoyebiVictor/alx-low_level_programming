2032d0383af9f9421ce8f414751784a32c0200ef
AtoyebiVictor <vickkypee2001@gmail.com>
2022-07-07 06:09:32 -0700

    Dont giveup

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * sum_them_all - returns the sum of all its parameter
+ * @n: integer variables
+ *
+ * Return: sum of its parameter
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list joy;
+	unsigned int k;
+	int sum = 0;
+
+	if (n == 0)
+		return 0;
+
+	va_start(joy, n);
+
+	for (k = 0; k <n; k++)
+		sum += va_arg(joy, int);
+
+	va_end(joy);
+
+	return (sum);
+}
