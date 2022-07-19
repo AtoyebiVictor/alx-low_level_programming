c5a249931f41b88db3c321c339fe312641d201e4
AtoyebiVictor <vickkypee2001@gmail.com>
2022-07-18 20:07:34 -0700

    Lets code

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * *add_nodeint - Adds a new node at the beginning
+ * @n: integer values
+ * @h: head of a list
+ *
+ * Return: Address of a new function or NULL
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *joy;
+
+	joy = malloc(sizeof(listint));
+
+	if (joy == NULL)
+		return (NULL);
+	joy->n = n;
+	joy->next = *head;
+	*head = joy;
+
+	return (*head);
+}
