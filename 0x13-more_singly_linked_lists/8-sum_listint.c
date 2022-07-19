ae497f8e1b840302ae836d1a6681da7f81cefea3
AtoyebiVictor <vickkypee2001@gmail.com>
2022-07-18 21:15:30 -0700

    Lets code

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * sum_listint - sum of all data(n) on a linked list
+ * @head: head of a node
+ *
+ * Return: 0
+ */
+int sum_listint(listint_t *head)
+{
+	int add = 0;
+
+	while(head != NULL)
+	{
+		add = add + head->n;
+		head = head->next;
+	}
+
+	return (add);
+}
