36bf2873925b0c6ed1f2f11e5e4093767718cee3
AtoyebiVictor <vickkypee2001@gmail.com>
2022-07-18 19:50:27 -0700

    Lets code

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+nclude "lists.h"
+
+/**
+ * print_listint - prints all the element
+ * @h: head of a list
+ *
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t kots = 0;
+
+	while (h != NULL)
+	{
+		orintf("%d", h->n);
+		h = h->next;
+		kots++;
+	}
+	return (kots);
+}
